b139fa94f1de81b66bef156cc170b2d31c82573a
Vipansh Thakur <vipansh@github.com>
2020-10-07 14:08:11 +0530

    Updates

---
+++ precache-manifest.95a82c397458ae6e3acdcbcb7e0df6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346cf88cce732c3929487652a41b3282",
+    "url": "/movies-list-app/index.html"
+  },
+  {
+    "revision": "36a5063f5125f7eafc96",
+    "url": "/movies-list-app/static/css/main.03397465.chunk.css"
+  },
+  {
+    "revision": "4fb693eec6a43bf53b0f",
+    "url": "/movies-list-app/static/js/2.931296e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies-list-app/static/js/2.931296e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a5063f5125f7eafc96",
+    "url": "/movies-list-app/static/js/main.43182544.chunk.js"
+  },
+  {
+    "revision": "d6285abb63cab73e613e",
+    "url": "/movies-list-app/static/js/runtime-main.47261e27.js"
+  }
+]);
